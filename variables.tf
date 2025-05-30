8f547dde3a65d8bdb6feb4b874639dc4e85f6c05
mythreyadevops <tirunagarimythreya@gmail.com>
2025-05-30 17:43:55 +0530

    yes

---
+++ variables.tf
@@ -0,0 +1,18 @@
+# variables.tf
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
